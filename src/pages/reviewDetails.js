75c78c22862fe27952f73d3db8c9bbbfc4d0595c
YDundie <mustacmarko@yahoo.com>
2021-09-13 20:01:52 +0200

    setup with rest

--- src/pages/reviewDetails.js
+++ src/pages/reviewDetails.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
+import { Link } from 'react-router-dom'
+
 
 export default function ReviewDetails() {
+    const { id } = useParams()
+    const { loading, error, data } = useFetch(`http://localhost:1337/reviews/${id}`)
+    console.log('data', data)
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error...</p>
     return (
-        <div>
-            Review Detail
+        <div className="review-card">
+            <Link to={`/`}>Go Back</Link>
+            <div className="rating">{data.rating}</div>
+            <h2>{data.title}</h2>
+            <small>console list</small>
+            <p>{data.body}</p>
+            <Link to={`/`}>Go Back</Link>
         </div>
     )
 }
