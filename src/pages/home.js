88962287991f241752d99c510c533e816aaf8eb1
YDundie <mustacmarko@yahoo.com>
2021-09-15 20:23:50 +0200

    few changes regarding graphql and added sorting

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useQuery, gql } from '@apollo/client'
+import { useState, useEffect } from "react";
 
 const REVIEWS = gql`
 query GetReviews {
@@ -9,26 +10,64 @@ query GetReviews {
       title
       rating
       body
+      categories{
+          name
+      }
     }
   }
 `
 
 export default function Home() {
+
+    const [sortDirection, setSortDirection] = useState(true)
+    const [dataDispaly, setDataDisplay] = useState([])
     const { loading, error, data } = useQuery(REVIEWS)
+
+    useEffect(() => {
+        if (data)
+            setDataDisplay(data.reviews.slice().sort((a, b) => {
+                return b.rating - a.rating
+            }))
+
+    }, [data])
+    useEffect(() => {
+        if (data)
+            if (sortDirection) {
+                setDataDisplay(data.reviews.slice().sort((a, b) => {
+                    return b.rating - a.rating
+                }))
+            } else {
+                setDataDisplay(data.reviews.slice().sort((a, b) => {
+                    return a.rating - b.rating
+                }))
+            }
+    }, [sortDirection])
 
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error...</p>
+
     return (
         <div>
-            {data.reviews.map(review => (
-                <div key={review.id} className="review-card">
-                    <div className="rating">{review.rating}</div>
-                    <h2>{review.title}</h2>
-                    <small>console list</small>
-                    <p>{review.body.substring(0, 200)}...</p>
-                    <Link to={`/details/${review.id}`}>Read More</Link>
-                </div>
-            ))}
-        </div>
+            <div className="review-card-for-button">
+
+                <button
+                    onClick={() => setSortDirection(!sortDirection)}
+                    className='sort-button'
+                >
+                    Sort
+            </button>
+            </div>
+            {
+                dataDispaly.map(review => (
+                    <div key={review.id} className="review-card">
+                        <div className="rating">{review.rating}</div>
+                        <h2>{review.title}</h2>
+                        <small>{review.categories.map(category => category.name).join()}</small>
+                        <p>{review.body.substring(0, 200)}...</p>
+                        <Link to={`/details/${review.id}`}>Read More</Link>
+                    </div>
+                ))
+            }
+        </div >
     )
 }
